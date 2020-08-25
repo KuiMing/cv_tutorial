2c55ccd677493e02322966568908f6dfafe70f2b
benjamin.chen <benjamin.chen@momagic.com>
2020-08-25 08:32:41 +0000

    upload file limitation

--- application.py
+++ application.py
@@ -5,7 +5,21 @@ from flask import Flask, flash, request, redirect, url_for, render_template
 from flask import send_from_directory
 from flask_caching import Cache
 
+
+
+
+def allowed_file(filename):
+    """
+    detect if file is allowed
+    """
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = ""
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+CATCHE = Cache(app, config={'CACHE_TYPE': 'null'})
+CATCHE.init_app(app)
 
 @app.route("/")
 def hello():
@@ -14,3 +28,4 @@ def hello():
 @app.route("/ben")
 def hellob():
     return render_template('upload.html')
+

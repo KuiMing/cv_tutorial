19e7a9e97058e5ac7184233b85a89dc74404c379
Benjamin Chen <benjamin.chen@momagic.com>
2020-08-28 12:00:26 +0800

    refactor

--- application.py
+++ application.py
@@ -1,16 +1,15 @@
+"""
+Object detection and image description on LINE bot
+"""
 import os
-import glob
-import time
-from flask import Flask, flash, request, redirect, url_for, render_template
-from flask import send_from_directory, abort
-
+import json
+from flask import Flask, request, abort
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
-import json
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage,
-                            FlexSendMessage, ImageMessage, ImageSendMessage)
+                            FlexSendMessage, ImageMessage)
 from imgur_python import Imgur
 from PIL import Image, ImageDraw, ImageFont
 
@@ -18,26 +17,28 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = ""
 
 with open('/home/config.json', 'r') as f:
-    config = json.load(f)
+    CONFIG = json.load(f)
 f.close()
 
-subscription_key = config['azure']['subscription_key']
-endpoint = config['azure']['endpoint']
-computervision_client = ComputerVisionClient(
-    endpoint, CognitiveServicesCredentials(subscription_key))
+SUBSCRIPTION_KEY = CONFIG['azure']['subscription_key']
+ENDPOINT = CONFIG['azure']['endpoint']
+CV_CLIENT = ComputerVisionClient(
+    ENDPOINT, CognitiveServicesCredentials(SUBSCRIPTION_KEY))
 
-line_secret = config['line']['line_secret']
-line_token = config['line']['line_token']
-line_bot_api = LineBotApi(line_token)
-handler = WebhookHandler(line_secret)
+LINE_SECRET = CONFIG['line']['line_secret']
+LINE_TOKEN = CONFIG['line']['line_token']
+LINE_BOT = LineBotApi(LINE_TOKEN)
+HANDLER = WebhookHandler(LINE_SECRET)
 
-imgur_config = config['imgur']
-imgur_client = Imgur(config=imgur_config)
+IMGUR_CONFIG = CONFIG['imgur']
+IMGUR_CLIENT = Imgur(config=IMGUR_CONFIG)
 
 
-def azure_describe(remote_image_url):
-    description_results = computervision_client.describe_image(
-        remote_image_url)
+def azure_describe(url):
+    """
+    Output azure image description result
+    """
+    description_results = CV_CLIENT.describe_image(url)
     output = ""
     for caption in description_results.captions:
         output += "'{}' with confidence {:.2f}% \n".format(
@@ -46,11 +47,14 @@ def azure_describe(remote_image_url):
 
 
 def azure_object_detection(url, filename):
+    """
+    Output azure image object detection result
+    """
     img = Image.open(filename)
     draw = ImageDraw.Draw(img)
-    fnt = ImageFont.truetype(
-        "static/TaipeiSansTCBeta-Regular.ttf", size=int(5e-2 * img.size[1]))
-    object_detection = computervision_client.detect_objects(url)
+    fnt = ImageFont.truetype("static/TaipeiSansTCBeta-Regular.ttf",
+                             size=int(5e-2 * img.size[1]))
+    object_detection = CV_CLIENT.detect_objects(url)
     if len(object_detection.objects) > 0:
         for obj in object_detection.objects:
             left = obj.rectangle.x
@@ -61,15 +65,15 @@ def azure_object_detection(url, filename):
             confidence = obj.confidence
             print("{} at location {}, {}, {}, {}".format(
                 name, left, right, top, bot))
-            draw.rectangle(
-                [left, top, right, bot], outline=(255, 0, 0), width=3)
-            draw.text(
-                [left, abs(top - 12)],
-                "{} {}".format(name, confidence),
-                fill=(255, 0, 0),
-                font=fnt)
+            draw.rectangle([left, top, right, bot],
+                           outline=(255, 0, 0),
+                           width=3)
+            draw.text([left, abs(top - 12)],
+                      "{} {}".format(name, confidence),
+                      fill=(255, 0, 0),
+                      font=fnt)
     img.save(filename)
-    image = imgur_client.image_upload(filename, 'first', 'first')
+    image = IMGUR_CLIENT.image_upload(filename, 'first', 'first')
     link = image['response']['data']['link']
     os.remove(filename)
     return link
@@ -77,18 +81,22 @@ def azure_object_detection(url, filename):
 
 @app.route("/")
 def hello():
+    "hello world"
     return "Hello World!!!!!"
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    """
+    LINE bot webhook callback
+    """
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     print(body)
     try:
-        handler.handle(body, signature)
+        HANDLER.handle(body, signature)
     except InvalidSignatureError:
         print(
             "Invalid signature. Please check your channel access token/channel secret."
@@ -97,37 +105,44 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@HANDLER.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    """
+    Reply text message
+    """
     message = TextSendMessage(text=event.message.text)
     print(event.source.user_id)
     print(event.source.type)
-    # print(line_bot_api.get_room_member_ids(room_id))
-    line_bot_api.reply_message(event.reply_token, message)
+    # print(LINE_BOT.get_room_member_ids(room_id))
+    LINE_BOT.reply_message(event.reply_token, message)
 
 
-@handler.add(MessageEvent, message=ImageMessage)
+@HANDLER.add(MessageEvent, message=ImageMessage)
 def handle_content_message(event):
+    """
+    Reply Image message with results of image description and objection detection
+    """
     if isinstance(event.message, ImageMessage):
         print(event.message)
         print(event.source.user_id)
         print(event.message.id)
         filename = "{}.jpg".format(event.message.id)
-        message_content = line_bot_api.get_message_content(event.message.id)
-        with open(filename, 'wb') as fd:
+        message_content = LINE_BOT.get_message_content(event.message.id)
+        with open(filename, 'wb') as f_w:
             for chunk in message_content.iter_content():
-                fd.write(chunk)
-        image = imgur_client.image_upload(filename, 'first', 'first')
+                f_w.write(chunk)
+        f_w.close()
+        image = IMGUR_CLIENT.image_upload(filename, 'first', 'first')
         link = image['response']['data']['link']
         output = azure_describe(link)
         link = azure_object_detection(link, filename)
-        with open('templates/detect_result.json', 'r') as f:
-            bubble = json.load(f)
-        f.close()
+        with open('templates/detect_result.json', 'r') as f_r:
+            bubble = json.load(f_r)
+        f_r.close()
         bubble['body']['contents'][0]['contents'][0]['contents'][0][
             'text'] = output
         bubble['header']['contents'][0]['contents'][0]['contents'][0][
             'url'] = link
-        line_bot_api.reply_message(
+        LINE_BOT.reply_message(
             event.reply_token,
             [FlexSendMessage(alt_text="Report", contents=bubble)])

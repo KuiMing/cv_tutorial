09a01abc4c854ecad516c73f7ad2a17d56d11622
Benjamin Chen <benjamin.chen@momagic.com>
2020-08-31 18:05:58 +0800

    face encoding

---
+++ face_pickle.py
@@ -0,0 +1,36 @@
+import glob
+import pickle
+import face_recognition
+import numpy as np
+from PIL import Image
+
+
+def main():
+    filenames = glob.glob('face_data/*/*')
+
+    face_data_names = []
+    face_data_encodings = []
+    for img_path in filenames:
+        name = img_path.split('/')[-2]
+        print('---')
+        print(name)
+        image = face_recognition.load_image_file(img_path)
+        height, width, _ = image.shape
+        image_resized = np.array(
+            Image.fromarray(image).resize((int(height / width * 500), 500)))
+        face_encodings = face_recognition.face_encodings(image_resized,
+                                                         num_jitters=4)
+        if len(face_encodings) > 0:
+            face_encoding = face_encodings[0]
+            face_data_names.append(name)
+            face_data_encodings.append(face_encoding)
+            print('{} is encoded'.format(img_path))
+        else:
+            print("No face detected in {}".format(img_path))
+    face_data = [face_data_names, face_data_encodings]
+    with open("face_data.pickle", "wb") as f:
+        pickle.dump(face_data, f)
+
+
+if __name__ == '__main__':
+    main()

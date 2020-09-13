a435f480a4a8842ad60fb5dcbc1a280aa967a9d1
KuiMing <benjamin0901@gmail.com>
2020-09-13 17:48:47 +0800

    facenet face recognition

--- face_recognizer.py
+++ face_recognizer.py
@@ -2,8 +2,94 @@ import pickle
 import cv2
 import face_recognition
 import dlib
+import numpy as np
+from keras.models import load_model
+from mtcnn import MTCNN
 
 # pylint: disable=maybe-no-member
+
+
+def label_face(frame, left, top, right, bottom, name):
+    cv2.rectangle(
+        frame, pt1=(left, top), pt2=(right, bottom), color=(0, 0, 255), thickness=2
+    )
+    cv2.rectangle(
+        frame,
+        pt1=(left, bottom - 35),
+        pt2=(right, bottom),
+        color=(0, 0, 255),
+        thickness=cv2.FILLED,
+    )
+    cv2.putText(
+        frame,
+        text=name,
+        org=(left + 6, bottom - 6),
+        fontFace=cv2.FONT_HERSHEY_DUPLEX,
+        fontScale=1.0,
+        color=(255, 255, 255),
+        thickness=1,
+    )
+
+
+class FacenetRecognition:
+    def __init__(self):
+        with open("face_data.pickle", "rb") as f_r:
+            self.names, self.encodings = pickle.load(f_r)
+        f_r.close()
+        self.image_size = 160
+        self.detector = MTCNN()
+        self.model = load_model("facenet_keras.h5")
+
+    def process_image(self, img):
+        face = np.array(img)
+        face_img = cv2.resize(face, (self.image_size, self.image_size))
+        face_img = self.prewhiten(face_img)
+        face_img = face_img[np.newaxis, :]
+        return face_img
+
+    def detect_face(self, frame, shrink):
+        face_encodings = []
+        face_locations = []
+        small_frame = cv2.resize(frame, (0, 0), fx=shrink, fy=shrink)
+        faces = self.detector.detect_faces(small_frame)
+        if len(faces) > 0:
+            for i in faces:
+                (left, top, width, height) = i["box"]
+                face_img = self.process_image(
+                    small_frame[top : top + height, left : left + width]
+                )
+                encoding = self.l2_normalize(
+                    np.concatenate(self.model.predict(face_img))
+                )
+                location = (left, top, left + width, top + height)
+                face_encodings.append(encoding)
+                face_locations.append(location)
+        return face_locations, face_encodings
+
+    def prewhiten(self, img):
+        mean = np.mean(img)
+        std = np.std(img)
+        std_adj = np.maximum(std, 1 / np.sqrt(img.size))
+        white_img = (img - mean) / std_adj
+        return white_img
+
+    def l2_normalize(self, x):
+        output = x / np.sqrt(
+            np.maximum(np.sum(np.square(x), axis=-1, keepdims=True), 1e-10)
+        )
+        return output
+
+    def recognize_face(self, frame, tolerance=0.5):
+        shrink = 0.25
+        face_locations, face_encodings = self.detect_face(frame, shrink)
+        for location, face_encoding in zip(face_locations, face_encodings):
+            left, top, right, bottom = [int(i / shrink) for i in location]
+            distances = np.linalg.norm(self.encodings - face_encoding, axis=1)
+            if min(distances) < tolerance:
+                name = self.names[distances.argmin()]
+            else:
+                name = "Unknown"
+            label_face(frame, left, top, right, bottom, name)
 
 
 class DlibFaceRecognition:
@@ -29,28 +115,6 @@ class DlibFaceRecognition:
             else:
                 name = "Unknown"
             label_face(frame, left, top, right, bottom, name)
-
-
-def label_face(frame, left, top, right, bottom, name):
-    cv2.rectangle(
-        frame, pt1=(left, top), pt2=(right, bottom), color=(0, 0, 255), thickness=2
-    )
-    cv2.rectangle(
-        frame,
-        pt1=(left, bottom - 35),
-        pt2=(right, bottom),
-        color=(0, 0, 255),
-        thickness=cv2.FILLED,
-    )
-    cv2.putText(
-        frame,
-        text=name,
-        org=(left + 6, bottom - 6),
-        fontFace=cv2.FONT_HERSHEY_DUPLEX,
-        fontScale=1.0,
-        color=(255, 255, 255),
-        thickness=1,
-    )
 
 
 class OpencvFaceRecognition:
@@ -82,7 +146,11 @@ class OpencvFaceRecognition:
             label_face(frame, left, top, right, bottom, name)
 
 
-RECOGNIZER = {"dlib": DlibFaceRecognition, "opencv": OpencvFaceRecognition}
+RECOGNIZER = {
+    "dlib": DlibFaceRecognition,
+    "facenet": FacenetRecognition,
+    "opencv": OpencvFaceRecognition,
+}
 
 
 def show_face():

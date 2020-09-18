563c9c3c29f59ec533425fcf77e294df2ad823e2
Benjamin Chen <benjamin.chen@momagic.com>
2020-09-18 19:08:22 +0800

    enhence front ground

--- background_remover.py
+++ background_remover.py
@@ -1,15 +1,19 @@
-import numpy as np
+import argparse
 import cv2
 
 
 def main():
-    cap = cv2.VideoCapture("race.mp4")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--video", help="video path", type=str)
+    args = parser.parse_args()
+    cap = cv2.VideoCapture(args.video)
     fgbg = cv2.createBackgroundSubtractorKNN()
     while True:
         ret, frame = cap.read()
         if ret:
             fgmask = fgbg.apply(frame)
             fgmask = fgmask / 255
+            fgmask = fgmask > 0
             for i in range(3):
                 frame[:, :, i] = frame[:, :, i] * fgmask
             cv2.imshow("frame", frame)

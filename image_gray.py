2dc7ec6fba783caa8a732d18fe9560aa8de13773
KuiMing <benjamin0901@gmail.com>
2020-09-18 09:45:29 +0800

    save image

--- image_gray.py
+++ image_gray.py
@@ -11,11 +11,8 @@ def main():
     # img = cv2.imread(args.image, flags=0)
     img = cv2.imread(args.image)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.namedWindow("image", cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty("image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    cv2.imshow("image", img)
-    cv2.moveWindow("image", 0, 0)
-    cv2.waitKey()
+
+    cv2.imwrite("img_gray.jpg", img)
 
 
 if __name__ == "__main__":

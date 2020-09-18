ac2d8feddb4aa6dfbb5ec101d26f25b351329f14
Benjamin Chen <benjamin.chen@momagic.com>
2020-09-18 19:09:49 +0800

    control kernel size

--- image_filter_vintage.py
+++ image_filter_vintage.py
@@ -2,21 +2,34 @@ import cv2
 import numpy as np
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--image", help="image path", type=str)
-args = parser.parse_args()
+# pylint: disable=maybe-no-member
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--image", help="image path", type=str)
+    args = parser.parse_args()
 
-img = cv2.imread(args.image)
-rows, cols = img.shape[:2]
+    size = 200
+    while True:
+        img = cv2.imread(args.image)
 
-kernel_x = cv2.getGaussianKernel(cols, 200)
-kernel_y = cv2.getGaussianKernel(rows, 200)
-kernel = kernel_y * kernel_x.T
-vintage_filter = 255 * kernel / np.linalg.norm(kernel)
-vintage_im = np.copy(img)
+        height, width = img.shape[:2]
+        kernel_x = cv2.getGaussianKernel(width, size)
+        kernel_y = cv2.getGaussianKernel(height, size)
+        kernel = kernel_y * kernel_x.T
+        vintage_filter = 255 * kernel / np.linalg.norm(kernel)
 
-for i in range(3):
-    vintage_im[:, :, i] = vintage_im[:, :, i] * vintage_filter
+        for i in range(3):
+            img[:, :, i] = img[:, :, i] * vintage_filter
+        cv2.imshow("vintage", img)
+        keyboard = cv2.waitKey(1)
+        # esc to quit
+        if keyboard == 27:
+            break
+        if chr(keyboard & 255) == "x":
+            size += 10
+        if chr(keyboard & 255) == "z":
+            size -= 10
 
-cv2.imshow("vintage", vintage_im)
-cv2.waitKey()
+
+if __name__ == "__main__":
+    main()

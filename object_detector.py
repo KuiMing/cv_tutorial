e7ba83ce34a1e1c5c6deca0f89d9ba478368981c
KuiMing <benjamin0901@gmail.com>
2020-09-20 11:25:20 +0800

    method for object detection

--- object_detector.py
+++ object_detector.py
@@ -10,53 +10,28 @@ with open(CLASSNAME, "r") as f:
     NAMES = f.read().rstrip("\n").split("\n")
 
 
-## Another way
-# net = cv2.dnn.readNet("yolov4.weights", "yolov4.cfg")
-## Fro Nvidia GPU
-# net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-## After cuda 7.5, it supports FP16.
-# net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
-# model = cv2.dnn_DetectionModel(net)
-# model.setInputSize(608, 608)
-# model.setInputScale(1 / 255)
-
-
 def detect_image(image, model):
     img = cv2.imread(image)
-    classes, confidences, locations = model.detect(
-        img, confThreshold=0.1, nmsThreshold=0.4
-    )
-    if len(classes) > 0:
-        result = zip(classes.flatten(), confidences.flatten(), locations)
-        for classId, confidence, location in result:
-            label = "%s: %.2f" % (NAMES[classId], confidence)
-            left, top, width, height = location
-            right = left + width
-            bottom = top + height
-            label_object(img, left, top, right, bottom, label)
-
+    detect_object(img, model)
     cv2.namedWindow("YOLO", cv2.WND_PROP_FULLSCREEN)
     cv2.setWindowProperty("YOLO", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.imshow("YOLO", img)
     cv2.waitKey()
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--image", help="image path", type=str)
-    parser.add_argument("-v", "--video", help="video path", type=str)
-    args = parser.parse_args()
-
-    model = cv2.dnn_DetectionModel("yolov4-tiny.cfg", "yolov4-tiny.weights")
-    model.setInputParams(size=(416, 416), scale=1 / 255)
-    if args.image:
-        detect_image(args.image, model)
-    else:
-        try:
-            video = int(args.video)
-        except:
-            video = args.video
-        detect_video(video, model)
+def detect_object(frame, model, shrink=1):
+    small_frame = cv2.resize(frame, dsize=(0, 0), fx=shrink, fy=shrink)
+    classes, confidences, locations = model.detect(
+        small_frame, confThreshold=0.1, nmsThreshold=0.4
+    )
+    if len(classes) > 0:
+        result = zip(classes.flatten(), confidences.flatten(), locations)
+        for classId, confidence, location in result:
+            label = "%s: %.2f" % (NAMES[classId], confidence)
+            left, top, width, height = [int(i / shrink) for i in location]
+            right = left + width
+            bottom = top + height
+            label_object(frame, left, top, right, bottom, label)
 
 
 def detect_video(video, model):
@@ -72,19 +47,7 @@ def detect_video(video, model):
         if mirror:
             frame = cv2.flip(frame, 1)
 
-        shrink = 0.25
-        small_frame = cv2.resize(frame, (0, 0), fx=shrink, fy=shrink)
-        classes, confidences, locations = model.detect(
-            small_frame, confThreshold=0.1, nmsThreshold=0.4
-        )
-        if len(classes) > 0:
-            result = zip(classes.flatten(), confidences.flatten(), locations)
-            for classId, confidence, location in result:
-                label = "%s: %.2f" % (NAMES[classId], confidence)
-                left, top, width, height = [int(i / shrink) for i in location]
-                right = left + width
-                bottom = top + height
-                label_object(frame, left, top, right, bottom, label)
+        detect_object(frame, model, 0.25)
 
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
         fps_info = "fps: {}".format(str(int(fps)))
@@ -99,6 +62,35 @@ def detect_video(video, model):
             break
         if chr(keyboard & 255) == "m":
             mirror = not mirror
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--image", help="image path", type=str)
+    parser.add_argument("-v", "--video", help="video path", type=str)
+    args = parser.parse_args()
+
+    model = cv2.dnn_DetectionModel("yolov4-tiny.cfg", "yolov4-tiny.weights")
+    model.setInputParams(size=(416, 416), scale=1 / 255)
+
+    ## Another way
+    # net = cv2.dnn.readNet("yolov4.weights", "yolov4.cfg")
+    ## Fro Nvidia GPU
+    # net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    ## After cuda 7.5, it supports FP16.
+    # net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+    # model = cv2.dnn_DetectionModel(net)
+    # model.setInputSize(416, 416)
+    # model.setInputScale(1 / 255)
+
+    if args.image:
+        detect_image(args.image, model)
+    else:
+        try:
+            video = int(args.video)
+        except:
+            video = args.video
+        detect_video(video, model)
 
 
 if __name__ == "__main__":
